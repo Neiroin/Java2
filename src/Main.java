6600da722a6c4fc576f579784902024947951ac0
Neiroin <plummy121@gmail.com>
2018-03-15 20:26:09 +0300

    alldone

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import by.belstu.it.krukovich.TextFunction;
+
 public class Main {
     //TODO hif2
     public static void main(String[] args) {
         System.out.println("Hello World!");
+     TextFunction tf = new TextFunction();
+     tf.onCreate();
     }
 }
--- src/by/belstu/it/krukovich/TextFunction.java
+++ src/by/belstu/it/krukovich/TextFunction.java
@@ -1,7 +1,25 @@
 package by.belstu.it.krukovich;
 
 public class TextFunction {
+    public TextFunction() {
+    }
+
     public String getString() {
         return "Hello from first project!";
     }
-}
+
+    public boolean deleteString() {
+        for (int i = 0; i < 9; i++) {
+            if (i > -1)
+                if (i < 10)
+                    System.out.println(i);
+        }
+        return true;
+    }
+    public void onCreate()
+    {
+        for (int count = 0; count < 10; count++) {
+            System.out.println("Counter "+count);
+        }
+    }
+    }
